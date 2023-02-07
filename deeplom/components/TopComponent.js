3d50b568dc3389d241c49875656d308a661224c1
Rus7Iv <rusivary@gmail.com>
2023-02-07 11:38:35 +0400

    Добавлен navbar, перенесён footer

--- deeplom/components/TopComponent.js
+++ deeplom/components/TopComponent.js
@@ -1,11 +1,12 @@
 import React from "react";
-import { Container } from "react-bootstrap";
+import { Container, Navbar } from "react-bootstrap";
 
 const TopComponent = () => {
   return (
     <>
       <div className="background"></div>
       <div className="texture">
+        <Navbar />
         <Container className="container-top">
           <h1>Hello!</h1>
         </Container>
